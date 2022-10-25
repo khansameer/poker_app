98e8f77d72ccfbf717d82bb72f5d9e7d77be2f73
Sameer Khan <pathansameerahmed@gmail.com>
2022-10-25 08:17:21 +0530

    new member details screen

--- lib/admin/features/memebers/admin_debit_page.dart
+++ lib/admin/features/memebers/admin_debit_page.dart
@@ -1,18 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:poker/core/common/common_text_widget.dart';
+import 'package:poker/core/utils/app_color.dart';
+import 'package:poker/core/utils/app_utils.dart';
+import 'package:poker/core/utils/image_path.dart';
+import 'package:poker/features/whaton/schedulebean.dart';
 
-class AdminDebitPage extends StatefulWidget{
+class AdminDebitPage extends StatefulWidget {
+  List<ScheduleBean> scheduleList;
+  AdminDebitPage(this.scheduleList);
+
+
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return AdminDebitPageState();
   }
-  
 }
-class AdminDebitPageState extends State<AdminDebitPage>{
+
+class AdminDebitPageState extends State<AdminDebitPage> {
+  //List<ScheduleBean> scheduleList = [];
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container();
+    return bindList();
   }
-  
+
+  Widget bindList() {
+    return ListView.builder(
+        shrinkWrap: true,
+        primary: false,
+        itemCount: widget.scheduleList.length,
+        padding: EdgeInsets.zero,
+        itemBuilder: (context, index) {
+          return AppUtils.commonBg(
+              padding1: AppUtils.commonAllEdgeInsets(left: 0,right: 0,bottom: 0,top: 0),
+              radius: 16,
+
+
+              borderWidth: 3,
+              padding: 0,
+
+              left: 0,
+
+              right: 0,
+              color: AppColor.colorBlueClub,
+              colorBorder: AppColor.colorWhiteLight,
+              widget: ListTile(
+                dense: false,
+                minVerticalPadding: 0,
+                contentPadding: EdgeInsets.only(left: 10,right: 10),
+                horizontalTitleGap: 10,
+
+                leading: SizedBox(
+                  width: 40,
+                  height: 40,
+                  child: AppUtils.commonImageAssetWidget(path: icGirlsIcon),
+                ),
+                trailing: AppUtils.commonInkWell(
+                    onTap: (){
+                      setState(() {
+                      //  scheduleList1.removeAt(index);
+                      });
+                    },
+                    child: CommonTextWidget(right:10,text: widget.scheduleList[index].text,textColor: AppColor.colorWhite,fontWeight: FontWeight.w800,)),
+                title: CommonTextWidget(text: "Player name",fontWeight: FontWeight.w800,),
+                subtitle: CommonTextWidget(text: "ID 00756",textColor: AppColor.colorWhite1,),
+              )
+          );
+        });
+  }
 }
--- lib/admin/features/memebers/memebers_page.dart
+++ lib/admin/features/memebers/memebers_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:poker/admin/features/memebers/admin_debit_page.dart';
 import 'package:poker/core/common/ToggleButton.dart';
 import 'package:poker/core/common/common_bg_page.dart';
 import 'package:poker/core/common/common_text_widget.dart';
@@ -7,6 +8,9 @@ import 'package:poker/core/utils/app_constants.dart';
 import 'package:poker/core/utils/app_utils.dart';
 import 'package:poker/core/utils/image_path.dart';
 import 'package:poker/core/utils/string_utils.dart';
+import 'package:poker/features/login/login_page.dart';
+import 'package:poker/features/signup/signup_page.dart';
+import 'package:poker/features/whaton/schedulebean.dart';
 
 class MembersPage extends StatefulWidget {
   const MembersPage({Key? key}) : super(key: key);
@@ -18,13 +22,42 @@ class MembersPage extends StatefulWidget {
 class _MembersPageState extends State<MembersPage> with SingleTickerProviderStateMixin {
 
   TabController? _tabController;
-
+  List<ScheduleBean> listCredit = [];
+  int selectedIndex=0;
+  List<ScheduleBean> listDebit = [];
   @override
   void initState() {
     _tabController = TabController(length: 2, vsync: this);
+
+    loadCredit();
+    loadDebit();
     super.initState();
   }
 
+  loadCredit() {
+    listCredit.add(ScheduleBean(
+      text: "2,500",));
+    listCredit.add(ScheduleBean(
+      text: "1,500",));
+    listCredit.add(ScheduleBean(
+      text: "2,000",));
+    listCredit.add(ScheduleBean(
+      text: "500",));
+    listCredit.add(ScheduleBean(
+      text: "541",));
+  }
+  loadDebit() {
+    listDebit.add(ScheduleBean(
+      text: "-500",));
+    listDebit.add(ScheduleBean(
+      text: "-500",));
+    listDebit.add(ScheduleBean(
+      text: "-500",));
+    listDebit.add(ScheduleBean(
+      text: "-500",));
+    listDebit.add(ScheduleBean(
+      text: "-500",));
+  }
   @override
   void dispose() {
     super.dispose();
@@ -36,110 +69,120 @@ class _MembersPageState extends State<MembersPage> with SingleTickerProviderStat
       onTap: () {
         FocusScope.of(context).unfocus();
       },
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppUtils.commonAppBar(
-            context: context,
-            fontSize: AppConstants.sixteen,
-            title: StringUtils.newMemberDetails,
-            isShowEdit: false,
-            actionTitle: StringUtils.edit),
-        body: Container(
-          decoration: AppUtils.containerDecoration(
-              color: AppColor.colorDarkBlue, radius: 0),
-          child: CommonBgPage(
-            backImagePath: icDashboardBg,
-            margin: AppConstants.fifteen,
-            alignment: Alignment.topCenter,
-            imagePath: icDashboardimg,
-            widget: Column(
-              children: [
-                membersList(),
-                Container(
-                  margin: EdgeInsets.only(top: 40),
-                  width: 300,
-                  decoration: BoxDecoration(
-                    color: AppColor.colorFillEditText,
-                    borderRadius: BorderRadius.circular(
-                      12.0,
-                    ),
-                  ),
-                  child: Container(
-                    margin: EdgeInsets.all(4),
-                    child: DefaultTabController(
-                      length: 2,
-                      child: TabBar(
+      child: CommonBgPage(
+        backImagePath: icDashboardBg,
+        margin: 0,
+        alignment: Alignment.topCenter,
+        imagePath: icDashboardimg,
+        widget: Scaffold(
+          backgroundColor:Colors.transparent,
+          resizeToAvoidBottomInset: false,
+          appBar: AppUtils.commonAppBar(
+              context: context,
+              fontSize: AppConstants.sixteen,
+              title: StringUtils.newMemberDetails,
+              isShowEdit: false,
+              actionTitle: StringUtils.edit),
+          body: SafeArea(
+            child: SingleChildScrollView(
+              child: Column(
 
-                        controller: _tabController,
-                        // give the indicator a decoration (color and border radius)
-                        indicator: BoxDecoration(
-                          borderRadius: BorderRadius.circular(
-                            12.0,
+                children: [
+                  Container(
+                    child: _gridView()
+                  ),
+                  Container(
+                    height:MediaQuery.of(context).size.height,
+                    margin: EdgeInsets.only(left: 20,right: 20,top:50 ),
+                  /*  decoration: AppUtils.containerDecoration(
+                        color: AppColor.colorDarkBlue, radius: 0),*/
+                    child:  Container(
+                      //margin: EdgeInsets.only(top: 40),
+
+                      child: Container(
+
+                        child: DefaultTabController(
+                          length: 2,
+                          initialIndex: 0,
+                          child: Expanded(
+                            flex: 1,
+                            child: Column(
+                              children: [
+                                Container(
+
+                                  decoration: BoxDecoration(
+                                    color: AppColor.colorFillEditText,
+                                    borderRadius: BorderRadius.circular(
+                                      12.0,
+                                    ),
+                                  ),
+                                  child: Container(
+                                    width: 300,
+                                    // margin: EdgeInsets.all(3),
+                                    child: TabBar(
+                                      onTap: (int index) {
+
+                                        selectedIndex = index;
+                                      },
+                                      padding: EdgeInsets.only(
+                                          left: AppConstants.five,
+                                          top: 5,
+                                          bottom: 5,
+                                          right: AppConstants.five),
+                                      controller: _tabController,
+                                      // give the indicator a decoration (color and border radius)
+                                      indicator: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(
+                                          12.0,
+                                        ),
+                                        color: AppColor.colorButton,
+                                      ),
+                                      unselectedLabelStyle: AppUtils.textStyle(fontSize: 14,fontWeight: FontWeight.w700) ,
+                                      labelStyle: AppUtils.textStyle(fontSize: 14,fontWeight: FontWeight.w700),
+                                      labelColor: Colors.white,
+                                      unselectedLabelColor: Colors.white,
+
+                                      tabs: [
+                                        // first tab [you can add an icon using the icon property]
+                                        Tab(
+                                          text: 'Debit List',
+                                        ),
+
+                                        // second tab [you can add an icon using the icon property]
+                                        Tab(
+                                          text: 'Credit List',
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(height: 30,),
+                                Expanded(
+                                  flex: 1,
+                                  child: TabBarView(
+                                    controller:_tabController ,
+                                    children: [
+                                      AdminDebitPage(listDebit),
+                                      AdminDebitPage(listCredit),
+                                      //BankDetailsPage(),
+                                      //TransactionHistoryPage(),
+                                    ],
+                                  ),
+                                )
+
+                              ],
+                            ),
                           ),
-                          color: AppColor.colorButton,
                         ),
-                        unselectedLabelStyle: AppUtils.textStyle(fontSize: 14,fontWeight: FontWeight.w700) ,
-                        labelStyle: AppUtils.textStyle(fontSize: 14,fontWeight: FontWeight.w700),
-                        labelColor: Colors.white,
-                        unselectedLabelColor: Colors.white,
-
-                        tabs: [
-                          // first tab [you can add an icon using the icon property]
-                          Tab(
-                            text: 'Debit List',
-                          ),
-
-                          // second tab [you can add an icon using the icon property]
-                          Tab(
-                            text: 'Credit List',
-                          ),
-                        ],
                       ),
-                    ),
+                    )
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
       ),
-    );
-  }
-
-  Widget membersList() {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        AppUtils.commonBg(
-          left: AppConstants.zero,
-          radius: AppConstants.sixteen,
-          colorBorder: AppColor.colorBorder,
-          borderWidth: 2,
-          widget: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                commonRow('Total Credit', '-65,541'),
-                AppUtils.commonSizedBox(height: AppConstants.ten),
-                commonRow('Total Debt', '6,541'),
-                AppUtils.commonSizedBox(height: AppConstants.ten),
-                commonRow('Total Owed', '-58,781'),
-              ]),
-        ),
-//////////////////////////////////////////////////
-        AppUtils.commonBg(
-          left: AppConstants.zero,
-          radius: AppConstants.sixteen,
-          colorBorder: AppColor.colorBorder,
-          borderWidth: 2,
-          widget: Column(mainAxisSize: MainAxisSize.min, children: [
-            commonRow('Host 1', '9,752'),
-            AppUtils.commonSizedBox(height: AppConstants.ten),
-            commonRow('Host 2', '3,390'),
-          ]),
-        ),
-      ],
     );
   }
 
@@ -167,6 +210,18 @@ class _MembersPageState extends State<MembersPage> with SingleTickerProviderStat
           ],
         ),
       ),
+    );
+  }
+  Widget _gridView() {
+    return GridView.count(
+      shrinkWrap: true,
+      crossAxisCount: 2,
+      padding: EdgeInsets.all(4.0),
+      childAspectRatio: 8.0 / 9.0,
+      children: [
+        FlutterLogo(),
+        FlutterLogo()
+      ]
     );
   }
 }
